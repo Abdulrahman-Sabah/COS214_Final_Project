ba0454c1cdac7041520db670d2341715e0b7413b
Josh-Heath123 <u23541475@tuks.co.za>
2025-10-18 07:17:33 +0200

    First implementation of Iterator Design Pattern

---
+++ Inventory.h
@@ -0,0 +1,26 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "Plant.h"
+#include "PlantIterator.h"
+#include <vector>
+
+
+class Inventory {
+private:
+    std::vector<Plant*> plants;
+
+public:
+
+    Inventory();
+    ~Inventory();
+    void addPlant(Plant* p);
+    bool removePlant(Plant* p);
+    bool removePlantByName(const std::string& name);
+    size_t getSize() const;
+    PlantIterator* createIterator();
+    std::vector<Plant*>& getPlants();
+    void displayAll();
+};
+
+#endif
