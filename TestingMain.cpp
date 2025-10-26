e01a946986556510d5c3af1184d3a97229d0f5ae
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2025-10-26 10:11:50 +0200

    Added_Clone_Testing

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -12,6 +12,9 @@
 #include "MoveToSales.h"
 #include "Command.h"
 #include "ScheduleDelivery.h"
+#include "ApplyFertilizer.h"
+#include "AddWater.h"
+
 
 int main() 
 {
@@ -32,6 +35,7 @@ int main()
 
     std::cout << "========== End of Factory Method Test ==========" << std::endl;
 
+    std::cout << std::endl;
 
     std::cout << "========== Plant Class Testing ==========\n" << std::endl;
 
@@ -39,7 +43,6 @@ int main()
     Lavender lavenderPlant;
     Rose rosePlant;
 
-    std::cout << "Name: " << cactus << std::endl;
     std::cout << "Description: " << cactusPlant.description() << std::endl;
     std::cout << "Care Type: " << cactusPlant.getCareType() << std::endl;
     std::cout << "State: " << cactusPlant.getStateText() << std::endl;
@@ -60,21 +63,53 @@ int main()
     std::cout << "Season: " << rosePlant.getSeason() << std::endl;
     std::cout << "Price: R" << rosePlant.price() << "\n" << std::endl;
 
+    std::cout << "========== Clone Tests ==========\n" << std::endl;
+
     Plant* clonedCactus = cactusPlant.clone();
-    std::cout << "--- Clone Test ---" << std::endl;
+    std::cout << "--- Cactus Clone ---" << std::endl;
     std::cout << "Cloned Name: " << clonedCactus->getName() << std::endl;
     std::cout << "Cloned Description: " << clonedCactus->description() << std::endl;
-    std::cout << "Cloned Price: R" << clonedCactus->price() << std::endl;
+    std::cout << "Cloned Care Type: " << clonedCactus->getCareType() << std::endl;
+    std::cout << "Cloned State: " << clonedCactus->getStateText() << std::endl;
+    std::cout << "Cloned Season: " << clonedCactus->getSeason() << std::endl;
+    std::cout << "Cloned Price: R" << clonedCactus->price() << "\n" << std::endl;
+
+    Plant* clonedLavender = lavenderPlant.clone();
+    std::cout << "--- Lavender Clone ---" << std::endl;
+    std::cout << "Cloned Name: " << clonedLavender->getName() << std::endl;
+    std::cout << "Cloned Description: " << clonedLavender->description() << std::endl;
+    std::cout << "Cloned Care Type: " << clonedLavender->getCareType() << std::endl;
+    std::cout << "Cloned State: " << clonedLavender->getStateText() << std::endl;
+    std::cout << "Cloned Season: " << clonedLavender->getSeason() << std::endl;
+    std::cout << "Cloned Price: R" << clonedLavender->price() << "\n" << std::endl;
+
+    Plant* clonedRose = rosePlant.clone();
+    std::cout << "--- Rose Clone ---" << std::endl;
+    std::cout << "Cloned Name: " << clonedRose->getName() << std::endl;
+    std::cout << "Cloned Description: " << clonedRose->description() << std::endl;
+    std::cout << "Cloned Care Type: " << clonedRose->getCareType() << std::endl;
+    std::cout << "Cloned State: " << clonedRose->getStateText() << std::endl;
+    std::cout << "Cloned Season: " << clonedRose->getSeason() << std::endl;
+    std::cout << "Cloned Price: R" << clonedRose->price() << "\n" << std::endl;
 
     delete clonedCactus;
+    delete clonedLavender;
+    delete clonedRose;
+
+    std::cout << "========== End of Clone Test ==========" << std::endl;
+
+    std::cout << std::endl;
 
     std::cout << "\n========== End of Plant Class Test ==========" << std::endl;
 
+    std::cout << std::endl;
 
     std::cout << "========== Command Pattern Testing ==========\n" << std::endl;
 
     MoveToSales moveCommand("Plant-001");
     ScheduleDelivery scheduleCommand("Order-ABC123");
+    ApplyFertilizer fertilizerCommand("Plant-002");
+    AddWater waterCommand("Plant-003");
 
     std::cout << "Command Type: " << moveCommand.getType() << std::endl;
     std::cout << "Command Data: " << moveCommand.getData() << "\n" << std::endl;
@@ -82,7 +117,14 @@ int main()
     std::cout << "Command Type: " << scheduleCommand.getType() << std::endl;
     std::cout << "Command Data: " << scheduleCommand.getData() << "\n" << std::endl;
 
+    std::cout << "Command Type: " << fertilizerCommand.getType() << std::endl;
+    std::cout << "Command Data: " << fertilizerCommand.getData() << "\n" << std::endl;
+
+    std::cout << "Command Type: " << waterCommand.getType() << std::endl;
+    std::cout << "Command Data: " << waterCommand.getData() << "\n" << std::endl;
+
     std::cout << "========== End of Command Test ==========" << std::endl;
+    std::cout << std::endl;
 
 
     return 0;
