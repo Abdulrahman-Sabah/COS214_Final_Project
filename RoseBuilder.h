acadbb28f65ddc08c77f6f985d8111a5a61cc280
DylanHebron <dylanellishebron@gmail.com>
2025-10-27 00:24:54 +0200

    Updated builders and changed succulent builder to cactus builder

--- RoseBuilder.h
+++ RoseBuilder.h
@@ -1,22 +1,31 @@
 #ifndef ROSE_BUILDER_H
 #define ROSE_BUILDER_H
 
+#include <string>
 #include "PlantBuilder.h"
+
+// Forward declarations to minimize coupling
+class Plant;
+class Rose;
+class PlantLifeCycleState;
 
 class RoseBuilder : public PlantBuilder
 {
     private:
-        Plant* rose;
+        Plant* product; // the plant being built (will be a Rose)
 
     public:
         RoseBuilder();
-        void setName(std::string n = "") override;
-        void setCareType(std::string c = "") override;
-        void setSeason(std::string s = "") override;
+        ~RoseBuilder() override;
+
+        // NOTE: By-value std::string parameters to match your ABI/linker traces
+        void setName(std::string n) override;
+        void setCareType(std::string ct) override;
+        void setSeason(std::string s) override;
         void setLifeCycle(PlantLifeCycleState* state) override;
-        void setCareStrategy(CareStrategy* strategy) override;
+        
+        // Transfer ownership of the built plant to the caller
         Plant* getPlant() override;
-        ~RoseBuilder();
 };
 
 #endif
