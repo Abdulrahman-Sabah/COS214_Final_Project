2cf3b8eebb6c220a69065d181917cec3808987f8
DylanHebron <dylanellishebron@gmail.com>
2025-10-27 23:26:20 +0200

    Added Receipt class for confirmation of customer purchase

---
+++ Receipt.cpp
@@ -0,0 +1,39 @@
+#include "Receipt.h"
+#include <iostream>
+#include <iomanip>
+
+void Receipt::addPlant(Plant* p)
+{
+    purchasedPlants.push_back(p);
+    totalAmount += p->price();
+}
+
+void Receipt::printReceipt() const
+{
+    std::cout << "\n====== RECEIPT ======\n";
+
+    // Printing Header
+    std::cout << std::left  << std::setw(20) << "Plant Name" 
+                            << std::setw(15) << "Care Type"
+                            << std::setw(10) << "Season"
+                            << std::setw(10) << "Price\n";
+    
+    std::cout << "---------------------------------------------\n";
+
+    // Printing each purchased plant
+    for(auto* p : purchasedPlants)
+    {
+        std::cout << std::left  << std::setw(20)                                << p->getName()
+                                << std::setw(15)                                << p->getCareType()
+                                << std::setw(10)                                << p->getSeason()
+                                << "R" << std::fixed << std::setprecision(2)    << p->price() << "\n";
+    }
+    
+    std::cout << "---------------------------------------------\n";
+
+    // Printing Footer
+
+    std::cout << "TOTAL: R" << totalAmount << std::endl;
+    std::cout << "Thank you for shopping with Git It Done Greenhouse!\n";
+    std::cout << "=====================\n";
+}
