6003c7f1125131c95c3ee31a52bb302c71b5d1a0
DylanHebron <dylanellishebron@gmail.com>
2025-10-28 07:51:09 +0200

    Added basic StaffFacade, for adding stock to inventory

---
+++ StaffFacade.h
@@ -0,0 +1,17 @@
+#ifndef STAFF_FACADE_H
+#define STAFF_FACADE_H
+
+#include "Inventory.h"
+#include <string>
+
+class StaffFacade
+{
+    private:
+        Inventory& inventory;
+
+    public:
+        explicit StaffFacade(Inventory& inv);
+        void addStock(const std::string& name);
+};
+
+#endif
