19e13f87a4f17ecf222e1f33280e46e511134eff
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2025-11-01 09:20:48 +0200

    Added_Extra_Output

--- src/SeedlingStateCmd.cpp
+++ src/SeedlingStateCmd.cpp
@@ -10,5 +10,6 @@ std::string SeedlingStateCmd::name() { return m_label; }
 void SeedlingStateCmd::execute(Plant* plant) {
   if (!plant) return;
   plant->setLifeCycle(new SeedlingState());
-  cout << "Cycle is being changed" << endl;
+      std::cout << "[COMMAND USED] " << m_label << " applied to "<< plant->getName() << "\n";
+
 }
